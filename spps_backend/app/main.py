779ba6df5584d11ca4c3be4fa4979839aef5322e
Mourjya Basu <mourjyabasu555@gmail.com>
2026-06-18 20:47:39 +0530

    Fix CORS

--- spps_backend/app/main.py
+++ spps_backend/app/main.py
@@ -2,8 +2,15 @@ from fastapi import FastAPI
 from app.database import Base, engine 
 from app import models 
 from routes import questions, results, admin, auth  
+from fastapi.middleware.cors import CORSMiddleware
 
 Base.metadata.create_all(bind=engine)
+
+app.add_middleware(CORSMiddleware, allow_origins=["http://localhost:3000",],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app = FastAPI()
 app.include_router(auth.router, prefix="/auth")
